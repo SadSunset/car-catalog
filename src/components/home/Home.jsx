0760632e852883a8978cb103a400656d6767c1fc
Alexander Semyahin <alexsander200121@gmail.com>
2023-09-24 22:17:25 +0300

    Delete useMemo hook

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,18 +1,15 @@
 import { cars } from '../../assets/mocks/cars.data'
 import { CarItem } from './car-item/CarItem'
-import { useMemo } from 'react'
 
 
 export const Home = () => {
-    const filteredCars = useMemo(() => 
-        cars.filter(car => car.price > 30000),[])
     
     return (
         <div>
             <h1>Home</h1>
             <div>
-                {filteredCars.length ?
-                    filteredCars.map(car =>
+                {cars.length ?
+                    cars.map(car =>
                         <CarItem
                             key={car.id}
                             car={car}
